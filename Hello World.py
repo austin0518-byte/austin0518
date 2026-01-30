d1126b918b68a368926966a65c39459f7d4dd7ff
ST-006\user <MyGit>
2026-01-30 11:20:57 +0800

    first

--- Hello World.py
+++ Hello World.py
@@ -1,3 +1,27 @@
-print("Hello World!!!!!!!!!!!!!!!!", end="")
-print("測試local與origin同步", end="")
-print("測試local", end="")
+k =60
+print(k)
+print(id(k))
+x = 30
+print(x)
+print(id(x))
+y = x
+print("y="+str(y))
+print("id(y)="+str(id(y)))
+result = x + y
+print("result="+str(result))
+print("id(result)="+str(id(result)))
+
+
+'''
+var1 = None
+var1 = 20
+var1 = 12.3
+var1 = "Hello!"
+var2 = 30
+var1 = var2
+result = var1 + var2
+result = var1 - var2
+result = var1 * var2
+result = var1 / var2
+result = var1 % var2
+'''
